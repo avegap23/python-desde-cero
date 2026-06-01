57c8c2c7975219e9c5402f23e7dd3f432b14b7f0
avegap23 <mancheganfur@proton.me>
2026-06-01 14:50:42 +0200

    01/06/2026

---
+++ ejemplo1.py
@@ -0,0 +1,33 @@
+# Comentario en una única línea
+
+'''
+Es un comentario multilínea, muy utilizado en documentación, claro está...
+'''
+
+# -----------------------------------------------
+
+'''
+Sí... ¿Pero qué es Python?
+    - Lenguaje interpretado, no compilado.. ¡No necesita ejecutable!
+    - Se programa en scripting
+    - Tiene un tipado dinámico, es decir, cuando "me da la gana", cambio de tipo de dato
+    - No se cambia de tipo "así por así", es decir, de forma repentina
+    - Es un lenguaje multiplataforma: Linux, Windows, MacOS y otros lo pueden ejecutar con o sin "ayudas"
+
+Empecemos con nomenclaturas "raras":
+
+    -> VARIABLE:
+        · Es un cajón que contiene información que quiero que contenga
+        · A lo largo de la vida del programa, puede cambiar (y cambia)
+    
+    -> CONSTANTE:
+        · Es un cajón cuyo contenido "no debería" variar en el ciclo de la vida del programa
+        · En Python se escribe: PI = 3.14159
+'''
+
+# Ejemplo basiquito total:
+nombre = 'Álvaro'
+
+print(nombre)
+
+print(type(nombre))
